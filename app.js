17c2f4cf8078bee7089fe29b7f9e7f874d866fce
Rasel <nodi.islam.5041@gmail.com>
2021-12-29 09:06:11 +0300

    generetorHex

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /**
+ * hex color genaretor and change color
+ * 
  * --steps:
 
  * step 1  create onload handler
@@ -20,21 +22,24 @@ window.onload = () => {
 function main() {
     const root = document.getElementById("root")
     const btn = document.getElementById("btn")
+    const output = document.getElementById("output")
+    
 
 
     btn.addEventListener('click', function () {
-        const bgColor = generetor()
+        const bgColor = generetorHex() 
         root.style.background = bgColor;
+        output.value=bgColor
     })
 }
 // step 2  rondom color generetor
-function generetor() {
+function generetorHex() {
     const red = Math.floor(Math.random() * 255)
     const green = Math.floor(Math.random() * 255)
     const blue = Math.floor(Math.random() * 255)
 
 
-    return `rgb(${red},${green}, ${blue})`
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
 }
 //step 3 collect all necessary refarencess
 
